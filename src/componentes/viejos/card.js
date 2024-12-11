b495406c1cd0979e0a34e8b470951876f8ed5b76
jaime <jaimegg93@educastur.es>
2024-12-11 13:52:15 +0100

    csacsa

--- src/componentes/viejos/card.js
+++ src/componentes/viejos/card.js
@@ -7,24 +7,16 @@ function Card() {
       }}
     >
       <div className="card-body">
-        <CardBody />
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>{" "}
       </div>
     </div>
-  );
-}
-
-function CardBody() {
-  return (
-    <>
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </p>
-      <a href="#" className="btn btn-primary">
-        Go somewhere
-      </a>
-    </>
   );
 }
 
